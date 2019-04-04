4e6c18bf133da7d840dae1be916a2da768300710
techfreks <48522802+techfreks@users.noreply.github.com>
2019-04-04 14:50:10 +0530

    Init setup

---
+++ breacherapp/breacherapp/src/test/java/com/src/breacherapp/BreacherappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.src.breacherapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BreacherappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
